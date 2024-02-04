9965dd08fd7b3fb260ab8f993b29f76a1fc9d527
최현주 <manensna6696@gmail.com>
2024-02-04 15:56:27 +0900

    도표추가 에러수정_디테일 완

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from "./pages/Detail/Dopyo_map";
+import App from './pages/DesktopOne/ShellWE';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
