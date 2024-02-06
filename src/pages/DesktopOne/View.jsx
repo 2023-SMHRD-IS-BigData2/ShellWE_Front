a6c899ae672ac49c73f48504218d7d5d7bc442bb
Ep <rep5sam8@gmail.com>
2024-02-06 12:26:47 +0900

    commit : Sidebar, Topbar, DarkMode

--- src/pages/DesktopOne/View.jsx
+++ src/pages/DesktopOne/View.jsx
@@ -1,34 +1,48 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Detail from '../Detail/Detail'
 import Main from '../Main/Main'
 import Admin from '../Admin/Admin'
 import { Route, Routes } from "react-router-dom";
 import axios from 'axios'
 import { PatientContext } from '../../context/PatientContext'
+import { ColorModeContext, useMode } from '../../theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Sidebar from '../Bar/Sidebar';
+import Topbar from '../Bar/Topbar';
 
 const View = () => {
- const {setPatient, setComment} = useContext(PatientContext);
-  useEffect(()=>{
+
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  const { setPatient, setComment } = useContext(PatientContext);
+  useEffect(() => {
     axios.get('http://localhost:3000/PatientList.json')
-    .then(res => setPatient(res.data.List[0]))
+      .then(res => setPatient(res.data.List[0]))
     axios.get('http://localhost:3000/Comment.json')
-    .then(res => setComment(res.data.List[0]))
-  },[])
-    
+      .then(res => setComment(res.data.List[0]))
+  }, [])
+
   return (
-    <div>
-      <Routes>
-        {/* 디테일 페이지 */}
-        <Route path='/detail' element={ <Detail />} />
-        {/* 메인 페이지 */}
-        <Route path='/main' element={ <Main />} />
-        {/* 관리자 페이지 */}
-        <Route path='/admin' element={ <Admin />} />
-      </Routes>
-      
-      
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              {/* 디테일 페이지 */}
+              <Route path='/detail' element={<Detail />} />
+              {/* 메인 페이지 */}
+              <Route path='/main' element={<Main />} />
+              {/* 관리자 페이지 */}
+              <Route path='/admin' element={<Admin />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
