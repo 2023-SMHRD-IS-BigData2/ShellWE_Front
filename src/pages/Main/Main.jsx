2e4af3cba2b30976e693fb23dc5821dc50c7e1eb
Hojun <dorajiji9942@gmail.com>
2024-02-13 09:38:15 +0900

    컴ㅅ

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
@@ -6,12 +6,28 @@ import { mockDataContacts } from "../../data/mockData";
 // import Header from "../../components/Header";
 import { useTheme } from "@mui/material";
 import columns from './columns.json';
+import axios from "axios";
+
 
 const Contacts = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
+  const [lists, setList] = useState(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8088/boot/getPatient");
+        setList(response.data[0]);
+        console.log("lists", response.data[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <Box m="20px">
@@ -45,8 +61,8 @@ const Contacts = () => {
           "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
             color: `${colors.grey[100]} !important`,
           },
-           // 스크롤 추가
-           overflow: "auto",
+          // 스크롤 추가
+          overflow: "auto",
         }}
       >
         <DataGrid
