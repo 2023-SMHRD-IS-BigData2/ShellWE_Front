59fcfb5183030f496e938829cfe4ee36cc4b89a0
Hojun <dorajiji9942@gmail.com>
2024-02-13 21:06:39 +0900

    중간 수정

--- src/pages/Main/Data.jsx
+++ src/pages/Main/Data.jsx
@@ -4,29 +4,47 @@ import { useTheme } from "@mui/material"
 import { Box, Typography } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import columns from './columns.json';
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 // 환자 데이터 컴포넌트
-const Data = ({lists}) => {
+const Data = ({ lists }) => {
 
     /** 다크모드 */
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
-    const navigate = useNavigate();
-   
+    // const navigate = useNavigate();
+
 
     // lists 값이 null인 경우 로딩 상태를 표시하거나 다른 방식으로 처리
     if (lists === null) {
         return <div>Loading...</div>;
     }
-    // 인덱스 1부터 시작
+    // // 누른 기준으로 디테일 넘기기
+    // const handleRowClick = (params) => {
+    //     const num = params.row.patinum;
+    //     navigate(`/main/detail/${num}`);
+    //     console.log("gesa");
+    // };
+
     const listsWithId = lists.map((list, index) => ({
         ...list,
         id: index + 1,
     }));
 
+
+
     const columnslist = [
         ...columns.columns,
+        {
+            field: "name",
+            headerName: "이름",
+            flex: 1,
+            renderCell: ({ row }) => (
+                <Link to={`/main/detail/${row.patinum}`}>
+                    {row.name}
+                </Link>
+            ),
+        },
         {
             field: "sepsisslevel",
             headerName: "패혈증 상태",
@@ -47,7 +65,6 @@ const Data = ({lists}) => {
                                     : "none"
                         }
                         borderRadius="4px"
-                    // onRowClick={}
                     >
                         <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
                             {sepsisslevel === "None" ? "" : sepsisslevel}
@@ -55,20 +72,15 @@ const Data = ({lists}) => {
                     </Box>
                 );
             },
-        }
-    ]
+        },
+    ];
 
-    const handleRowClick = (params) => {
-        const num = params.row.patinum;
-        navigate(`/main/detail/${num}`);
-    };
 
     return (
         <DataGrid
             rows={listsWithId}                       /** 환자 데이터 */
             columns={columnslist}                /** 컬럼명 */
             components={{ Toolbar: GridToolbar }}    /** 필터 기능 (다운로드, 크기 조절) */
-            onRowClick={handleRowClick} // 각 행 클릭 시 핸들러 호출
         >
         </DataGrid>
     )
