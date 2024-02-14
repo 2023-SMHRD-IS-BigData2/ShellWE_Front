417814bb4e20be2fb2abd8866576b11fd240c683
Hojun <dorajiji9942@gmail.com>
2024-02-14 09:58:02 +0900

    디테일로 넘기는 부분 이름클릭으로 수정

--- src/pages/Main/Data.jsx
+++ src/pages/Main/Data.jsx
@@ -1,50 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { tokens } from "../../theme";
 import { useTheme } from "@mui/material"
 import { Box, Typography } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import columns from './columns.json';
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 // 환자 데이터 컴포넌트
-const Data = ({ lists }) => {
+const Data = () => {
 
     /** 다크모드 */
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
+    /** 환자 리스트 */
+    const [lists, setList] = useState(null);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("http://localhost:8088/boot/getPatient");
+                setList(response.data.patientList);
+                console.log("lists", response.data.patientList);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     // lists 값이 null인 경우 로딩 상태를 표시하거나 다른 방식으로 처리
     if (lists === null) {
         return <div>Loading...</div>;
     }
-    // // 누른 기준으로 디테일 넘기기
-    // const handleRowClick = (params) => {
-    //     const num = params.row.patinum;
-    //     navigate(`/main/detail/${num}`);
-    //     console.log("gesa");
-    // };
-
+    // 인덱스 1부터 시작
     const listsWithId = lists.map((list, index) => ({
         ...list,
         id: index + 1,
     }));
 
-
-
     const columnslist = [
-        ...columns.columns,
         {
-            field: "name",
-            headerName: "이름",
+            field: "Name",
+            headerName: "환자명",
             flex: 1,
             renderCell: ({ row }) => (
-                <Link to={`/main/detail/${row.patinum}`}>
+                <Link key={row.id} to={`/main/detail/${row.patinum}`} style={{ textDecoration: 'none', color: 'White' }}>
                     {row.name}
                 </Link>
             ),
         },
+        ...columns.columns.filter((column) => column.field !== "name"),
         {
             field: "sepsisslevel",
             headerName: "패혈증 상태",
@@ -75,12 +83,17 @@ const Data = ({ lists }) => {
         },
     ];
 
+    const handleRowClick = (params) => {
+        const num = params.row.patinum;
+        navigate(`/main/detail/${num}`);
+    };
 
     return (
         <DataGrid
             rows={listsWithId}                       /** 환자 데이터 */
             columns={columnslist}                /** 컬럼명 */
             components={{ Toolbar: GridToolbar }}    /** 필터 기능 (다운로드, 크기 조절) */
+            onRowClick={handleRowClick} // 각 행 클릭 시 핸들러 호출
         >
         </DataGrid>
     )
