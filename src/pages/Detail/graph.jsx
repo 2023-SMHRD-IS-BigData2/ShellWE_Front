2ab24bc0b8220c2acfbcd804fe0a6f5b85d37e7c
Hojun <dorajiji9942@gmail.com>
2024-02-19 17:45:22 +0900

    그래프 부분수정

--- src/pages/Detail/graph.jsx
+++ src/pages/Detail/graph.jsx
@@ -1,8 +1,14 @@
 import React, { useContext } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer ,Brush} from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, ResponsiveContainer, Brush } from 'recharts';
 import { ChartContext } from './ChartContext';
 import { Box, Typography, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
+
+const CustomizedLabel = ({ x, y, value }) => {
+    return (
+        <text x={x+8} y={y+6} dy={-10} fontSize={12} textAnchor="middle" fill="red">{value}</text>
+    );
+};
 
 const Graph = () => {
 
@@ -52,18 +58,17 @@ const Graph = () => {
                         fontWeight="bold"
                         color={colors.greenAccent[500]}
                     >
-                         {data[data.length - 1][subtitle]}
+                        {data[data.length - 1][subtitle]}
                     </Typography>
                 </Box>
             </Box>
             <Box height="250px" m="-20px 0 0 0" >
                 <ResponsiveContainer width="100%" height={300}>
                     <LineChart width="100%" height={300} data={filteredData}
-                        margin={{ top: 20, bottom: 40, right: 20, left: 80 }}>
+                        margin={{ top: 20, bottom: 40, right: 20, left: 35 }}>
                         <CartesianGrid vertical={false} strokeOpacity={0.3} />
                         <XAxis dataKey="time" />
-                        <YAxis yAxisId={graph} dataKey={graph} orientation="right" />
-                        <Tooltip />
+                        <YAxis yAxisId={graph} dataKey={graph} orientation="left"/>
                         <Line type="linear" dataKey={graph} stroke="red" yAxisId={graph} />
                         <Brush
                             dataKey="time"
@@ -74,9 +79,15 @@ const Graph = () => {
                             opacity="0.2"
                             fillOpacity={0.2}
                         />
+                        <Line
+                            type="linear"
+                            dataKey={graph}
+                            stroke="red"
+                            yAxisId={graph}
+                            label={<CustomizedLabel />}
+                        />
                     </LineChart>
                 </ResponsiveContainer>
-
             </Box>
         </Box>
     );
